67d06ef33a4335748e1e35a03e71d08ee13e36a6
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2025-07-30 19:57:56 -0400

    Removed redundant csv to dicts

--- App/main.py
+++ App/main.py
@@ -3,35 +3,20 @@ import models.Order as Order
 
 CSV_FILE_NAME = 'Data/Order List.csv'
 
-def get_rows(file_name):
-    rows = []
+def read_csv_as_dicts(file_name):
     with open(file_name, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in reader:
-            rows.append(row)
-    return rows
-
-def convert_rows_to_dict(rows):
-    header = rows[0]
-    new_dict = []
-    for row in rows[1:]:
-        new_object = {}
-        for column_index, value in enumerate(row):
-            column_header = header[column_index]
-            new_object[column_header] = value
-        new_dict.append(new_object)
-    return new_dict
+        reader = csv.DictReader(csvfile)
+        return list(reader)
 
 def convert_dicts_to_orders(dicts):
     orders = []
-    for dict in dicts:
-        order = Order.Order(dict)
+    for row in dicts:
+        order = Order.Order(row)
         orders.append(order)
     return orders
 
 def main():
-    rows = get_rows(CSV_FILE_NAME)
-    dicts = convert_rows_to_dict(rows)
+    dicts = read_csv_as_dicts(CSV_FILE_NAME)
     orders = convert_dicts_to_orders(dicts)
     print(orders[0])
 
